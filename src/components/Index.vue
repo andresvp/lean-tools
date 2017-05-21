5ead3b36d668cd6b765f59831be4e4c284ae3d69
matheus-rossi <matheus_rossi@outlook.com>
2017-05-21 18:07:51 -0300

    fix drawer

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,9 +1,18 @@
 <template>
-
+    <q-layout>
+        <app-Header></app-Header>
+        <router-view class="layout-view"></router-view>
+    </q-layout>
 </template>
 
 <script>
+import Header from './Header.vue'
 
+export default {
+  components: {
+    appHeader: Header
+  }
+}
 </script>
 
 <style>

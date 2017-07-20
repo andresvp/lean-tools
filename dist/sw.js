474e80266d954eeb243aa506252b4d6a80ac239b
Matheus Rossi <matheus_rossi@outlook.com>
2017-07-19 21:07:36 -0300

    sw adding pages

--- dist/sw.js
+++ dist/sw.js
@@ -3,6 +3,13 @@ self.addEventListener('install', function(e) {
    caches.open('airhorner').then(function(cache) {
      return cache.addAll([
        '/',
+       '/index.html',
+       '/server.js',
+       '/sw.js',
+       '/app.3c5e57d50d8fb774299414013c5ae958.css',
+       '/js/app.js',
+       '/js/manifest.js',
+       '/js/vendor.js'
      ])
    })
  )

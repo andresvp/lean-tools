e0cf3a444a4e971daf0d3d057cb625d095f27603
Matheus Rossi <matheus_rossi@outlook.com>
2017-07-21 21:18:34 -0300

    forcing https

--- dist/server.js
+++ dist/server.js
@@ -1,21 +1,12 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var secure = require('express-force-https');
+
 app = express();
 app.use(serveStatic(__dirname));
-app.use(function (req, res, next) {
-  var newURL;
+app.use(secure);
 
-  // If not on HTTPS, or not on the main domain, redirect
-  if (process.env.NODE_ENV === 'production' &&
-    (req.headers['x-forwarded-proto'] !== 'https' || req.headers.host !== 'hjnilsson.com')) {
-
-    newURL = ['https://hjnilsson.com', req.url].join('');
-    return res.redirect(newURL);
-  }
-
-  return next();
-});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
